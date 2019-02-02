9ab8903d0834c11a1842e78e5ab0997026f06ddc
yangzongfang <yangzongfang@100tal.com>
2019-02-02 15:49:00 +0800

    1

--- src/main.go
+++ src/main.go
@@ -3,13 +3,14 @@ package main
 import (
 	"net/http"
 	"impl"
+	_ "net/http/pprof"
 )
 
 func main() {
 	hub := impl.NewHub()
 	go hub.Run()
 	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
-		impl.Handler(hub,w,r)
+		impl.Handler(hub, w, r)
 	})
 	impl.SystemOutput.Info("服务开启成功！")
 	http.ListenAndServe(":7777", nil)
